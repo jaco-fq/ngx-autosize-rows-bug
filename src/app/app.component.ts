53b1c90a2c5215fdbab1dacbcef00e6bddf1328c
Jaco ter Braak <jaco.ter.braak@fabriquartz.com>
2020-03-12 21:44:33 +0100

    Update to bug manifestation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,13 @@
-import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {Component, ViewChild} from '@angular/core';
+import {Nav} from 'ionic-angular';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  templateUrl: 'app.html'
 })
-export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
+export class NgxAutosizeRowsBug {
+  @ViewChild(Nav) public nav: Nav;
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
+  public constructor() {
   }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
-
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {Config, IonicApp, IonicModule} from 'ionic-angular';
+import {NgxAutosizeRowsBug} from './app.component';
+import {AutosizeModule} from "ngx-autosize";
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  declarations: [
+    NgxAutosizeRowsBug
   ],
-  bootstrap: [AppComponent]
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(NgxAutosizeRowsBug, {
+      preloadModules: true
+    }),
+    AutosizeModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    NgxAutosizeRowsBug
+  ],
+  providers: [
+  ]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(public config: Config) {
+  }
+}
